7e34d562bb137cffabd2ff9826cc7fc21a3a4454
Rafael Anacleto <rafael.anacleto12@gmail.com>
2024-10-18 11:19:28 -0300

    feat: add contact-list component and route to contact-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { ContactListComponent } from './pages/contact-list/contact-list.component';
 import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [{ path: 'login', component: LoginComponent }];
+export const routes: Routes = [{ path: 'login', component: LoginComponent }, { path: '', component: ContactListComponent}];
