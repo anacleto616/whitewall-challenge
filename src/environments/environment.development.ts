47134f66b82ce7bf0a0bd577df93c9bd3d146a0e
Rafael Anacleto <rafael.anacleto12@gmail.com>
2024-10-18 14:26:17 -0300

    chore: add environments files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://rafael-anacleto-gupe5.http.msging.net/commands',
+  apiKey: 'Key YW5hY2xldG9ib3Q6TE5RUG9lYlFxWGdIcEJjTjljUGE=',
+  apiId: '8b9d7aef-b8f9-42ec-9125-08841992ea50',
+};
